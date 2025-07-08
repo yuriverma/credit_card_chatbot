c320e8e1de97ed4a0710cad45beb9f60b9ee66df
Yuri Verma <136191160+yuriverma@users.noreply.github.com>
2025-07-08 14:40:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 from dotenv import load_dotenv
 from datetime import datetime
-from zoneinfo import ZoneInfo  # use pytz if < Python 3.9
+from zoneinfo import ZoneInfo  
 import uuid
 
 
